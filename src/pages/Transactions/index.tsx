0a36f7a3146c12cb0804e4f1367050ecb3d3e6b4
Lucas Roseti <lc.roseti@gmail.com>
2023-02-07 22:09:28 -0300

    Transactions table
    Transactions table code and CSS

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,45 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
-import { TransactionsContainer } from './styles'
+import {
+  TransactionsContainer,
+  TransactionsContent,
+  TransactionsPriceHighlight,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <TransactionsContainer>
       <Header />
       <Summary />
+
+      <TransactionsContent>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <TransactionsPriceHighlight variant="income">
+                  $ 12.000,00
+                </TransactionsPriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <TransactionsPriceHighlight variant="outcome">
+                  - R$ 59,00
+                </TransactionsPriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContent>
     </TransactionsContainer>
   )
 }
